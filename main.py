880211ba56a6c54cd553c7a9850d67845e1762b1
Tiago Costa <108727395+tiagocncosta@users.noreply.github.com>
2024-05-08 15:39:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,62 @@
-from fastapi import FastAPI
+import os
+
+from fastapi import FastAPI, Depends
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from starlette.responses import RedirectResponse
 import boto3
 from pydantic import BaseModel
+from fastapi import Body, HTTPException
+import uvicorn
 
-app = FastAPI()
 # Replace placeholders with your actual values
-LEX_PROFILE_NAME = "prod@account-admin"
 LEX_REGION_NAME = "eu-central-1"
 LEX_BOT_ID = "18HYOHEXKA"
 LEX_BOT_ALIAS_ID = "TSTALIASID"
-LEX_SESSION_ID = "100"
+
+
+app = FastAPI(
+    title="Service Desk Chatbot",
+    description="Service Desk Chatbot"
+)
+
+bearer_scheme = HTTPBearer()
+BEARER_TOKEN = "1234" #os.environ.get("BEARER_TOKEN")
+assert BEARER_TOKEN is not None
+
+
+def validate_token(credentials: HTTPAuthorizationCredentials = Depends(bearer_scheme)):
+    if credentials.scheme != "Bearer" or credentials.credentials != BEARER_TOKEN:
+        raise HTTPException(status_code=401, detail="Invalid or missing token")
+    return credentials
+
+
+app = FastAPI(dependencies=[Depends(validate_token)])
 
 
 class Item(BaseModel):
     sessionId: str
     text: str
 
-@app.get("/")
+# auxiliary requests for framework purposes following...
+# redirect root requests to the docs
+@app.get("/", include_in_schema=False)
 def root():
-    """Returns a welcome message."""
-
-    return {"message": "Welcome to the Amazon Lexv2 API!"}
+    return RedirectResponse(url="/docs")
 
 @app.post("/")
 def root(item: Item):
     """Sends user text to Amazon Lexv2 for processing."""
 
     try:
-        # Create a boto3 session with the specified profile
+        
+        client = session.client('lexv2-runtime', region_name=LEX_REGION_NAME)
 
-        # Create Lexv2 client
-        client = boto3.client('lexv2-runtime', region_name=LEX_REGION_NAME)
-
-        # Send the text to Lexv2 for processing
+        # Send the text to Lex for processing
         response = client.recognize_text(
             botId=LEX_BOT_ID,
             botAliasId=LEX_BOT_ALIAS_ID,
             localeId="en_US",  # Consider making this dynamic based on user input
-            sessionId=LEX_SESSION_ID,
+            sessionId=item.sessionId,
             text=item.text
         )
 
@@ -45,9 +65,20 @@ def root(item: Item):
         if "errorMessage" in response:
             return {"error": response["errorMessage"]}
 
-        return response
+        return response['messages']['content']
 
     except Exception as e:
         # Log the error for deeper troubleshooting
         print(f"An error occurred: {e}")
         return {"error": "Internal server error"}
+
+def main():
+    # noinspection PyTypeChecker
+    port = 8000
+    print(f"Starting application, once loaded, access at 3.122.241.50/:{port}")
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
+if __name__ == "__main__":
+    main()
+
